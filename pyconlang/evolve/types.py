cc687bfccc55911d505cee6e65d6f1e4d380ae23
Neta Elad <elad.neta@gmail.com>
2022-08-23 09:50:59 -0700

    repl help

--- pyconlang/evolve/types.py
+++ pyconlang/evolve/types.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
 
+from unidecode import unidecode
+
 
 @dataclass(eq=True, frozen=True)
 class Evolved:
     proto: str
     modern: str
     phonetic: str
+
+    @property
+    def simple(self) -> str:
+        return unidecode(self.modern)
--- pyconlang/repl.py
+++ pyconlang/repl.py
@@ -1,13 +1,15 @@
 from cmd import Cmd
 from dataclasses import dataclass, field
+from operator import attrgetter
+from typing import Callable
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
-from unidecode import unidecode
 from watchdog.events import FileSystemEvent, PatternMatchingEventHandler
 from watchdog.observers import Observer
 
 from . import PYCONLANG_PATH
+from .evolve.types import Evolved
 from .translate import Translator
 
 HISTORY_PATH = PYCONLANG_PATH / "repl.history"
@@ -33,6 +35,9 @@ class ReplSession(Cmd):
     translator: Translator = field(default_factory=Translator)
     session: PromptSession[str] = field(default_factory=_default_prompt_session)
     watcher: Handler = field(default_factory=Handler)
+
+    def __post_init__(self) -> None:
+        super().__init__()
 
     def run(self) -> None:
         observer = Observer()
@@ -63,25 +68,50 @@ class ReplSession(Cmd):
         try:
             self.onecmd(line)
         except Exception as e:
-            print(f"Bad command: {repr(e)}")
+            print(f"Error while running command: {repr(e)}")
 
-    def default(self, line: str) -> None:
-        print(" ".join(form.modern for form in self.translator.evolve_string(line)))
+    def translate(
+        self, line: str, getter: Callable[[Evolved], str] = attrgetter("modern")
+    ) -> None:
+        print(" ".join(getter(form) for form in self.translator.evolve_string(line)))
+
+    def default(
+        self,
+        line: str,
+    ) -> None:
+        self.translate(line)
+
+    def do_default(self, line: str) -> None:
+        """
+        Translates a sentence using the lexicon and sound changes.
+        Can also be used without prefixing the command.
+        """
+        return self.default(line)
+
+    def do_d(self, line: str) -> None:
+        """
+        See default.
+        """
+        return self.do_default(line)
 
     def do_phonetic(self, line: str) -> None:
-        print(" ".join(form.phonetic for form in self.translator.evolve_string(line)))
+        """Returns the phonetic forms of the translated sentence."""
+        self.translate(line, attrgetter("phonetic"))
 
     def do_p(self, line: str) -> None:
+        """See phonetic."""
         self.do_phonetic(line)
 
     def do_simple(self, line: str) -> None:
-        print(
-            " ".join(
-                unidecode(form.modern) for form in self.translator.evolve_string(line)
-            )
-        )
+        """
+        Simplifies the forms of the translated sentence into basic ascii.
+        """
+        self.translate(line, attrgetter("simple"))
 
     def do_s(self, line: str) -> None:
+        """
+        See simple.
+        """
         self.do_simple(line)
 
 
