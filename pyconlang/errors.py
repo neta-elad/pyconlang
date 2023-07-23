e10e3714b1692990d4fb570eaebd0e051183bd1a
Neta Elad <elad.neta@gmail.com>
2023-07-23 14:31:01 +0300

    remove dangling import of pyparsing

--- pyconlang/errors.py
+++ pyconlang/errors.py
@@ -1,6 +1,3 @@
-from pyparsing import ParseBaseException
-
-
 class PyconlangError(Exception):
     ...
 
@@ -10,8 +7,4 @@ class DoubleTagDefinition(PyconlangError):
 
 
 def show_exception(exception: Exception) -> str:
-    match exception:
-        case ParseBaseException():
-            return exception.explain(depth=0)
-        case _:
-            return f"{type(exception).__name__}: {exception}"
+    return f"{type(exception).__name__}: {exception}"
